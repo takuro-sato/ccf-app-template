8f8892a3a5e7a6257bdf6558b29e7c8e3ec31fda
Takuro Sato <takurosato@microsoft.com>
2022-09-28 13:23:02 +0000

    Try to add an endpoint 'double'
    After starting sandbox, you can try it with:
    $ cd banking-app/workspace/sandbox_common
    $ curl https://127.0.0.1:8000/app/double -X POST --cacert service_cert.pem --cert user0_cert.pem --key user0_privk.pem -H "Content-Type: application/json" --data-binary '[0,1,2,3,4,5,6,7,8,9]'

--- banking-app/src/endpoints/all.ts
+++ banking-app/src/endpoints/all.ts
@@ -2,6 +2,7 @@ export * from "./jwt";
 export * from "./crypto";
 export * from "./oe";
 export * from "./partition";
+export * from "./double";
 export * from "./proto";
 export * from "./log";
 export * from "./rpc";
